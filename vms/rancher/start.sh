b2679fd4553fc8a329b1340a21aa83893fc8b710
Jeroen van Erp <jeroen.vanerp@suse.com>
2025-08-15 15:53:15 +0200

    echo some stuff

--- vms/rancher/start.sh
+++ vms/rancher/start.sh
@@ -8,8 +8,14 @@ do
   sleep 1
 done
 
+echo "PATH: $PATH"
+echo "User: $(whoami)"
+echo "UID: $(id -u)"
+echo "GID: $(id -g)"
+
+
 # makes sure the registration is ok
-registercloudguest --force-new
+# registercloudguest --force-new
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
@@ -44,7 +50,7 @@ fi
 # Wait for kubernetes to be running
 echo ">>> Waiting for kubernetes to be running"
 for i in {1..60}; do
-  if kubectl cluster-info &>/dev/null; then
+  if /usr/bin/kubectl cluster-info &>/dev/null; then
     echo "Kubernetes is running"
     break
   fi
@@ -54,10 +60,10 @@ done
 
 # Wait for cert-manager
 echo ">>> Waiting for cert-manager to be ready"
-kubectl wait --for=condition=Ready pod -l app.kubernetes.io/instance=cert-manager -n cert-manager --timeout=300s
+/usr/bin/kubectl wait --for=condition=Ready pod -l app.kubernetes.io/instance=cert-manager -n cert-manager --timeout=300s
 
-kubectl patch ingress rancher -n cattle-system --type='json' -p='[{"op": "replace", "path": "/spec/rules/0/host", "value": "'"${RANCHER_DOMAIN}"'"}, {"op": "replace", "path": "/spec/tls/0/hosts/0", "value": "'"${RANCHER_DOMAIN}"'"}]'
+/usr/bin/kubectl patch ingress rancher -n cattle-system --type='json' -p='[{"op": "replace", "path": "/spec/rules/0/host", "value": "'"${RANCHER_DOMAIN}"'"}, {"op": "replace", "path": "/spec/tls/0/hosts/0", "value": "'"${RANCHER_DOMAIN}"'"}]'
 # Wait for certificate to be issued
-kubectl wait --for=condition=Ready certificate rancher-tls -n cattle-system --timeout=300s
+/usr/bin/kubectl wait --for=condition=Ready certificate rancher-tls -n cattle-system --timeout=300s
 
 rancher_first_login $RANCHER_URL $RANCHER_ADMIN_PASSWORD
