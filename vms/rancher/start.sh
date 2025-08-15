939113584e985334dc46f3d207d66932d657be56
Jeroen van Erp <jeroen.vanerp@suse.com>
2025-08-15 10:39:58 +0200

    Rename setup to provision and add start script

---
+++ vms/rancher/start.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# This script is used during the Instruqt track_scripts phase to configure the pre-provisioned Rancher for the track instance.
+
+# waits for Instruqt host bootstrap to finish
+until [ -f /opt/instruqt/bootstrap/host-bootstrap-completed ]
+do
+  sleep 1
+done
+
+# makes sure the registration is ok
+registercloudguest --force-new
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+echo "Script directory: ${SCRIPT_DIR}"
+
+. $SCRIPT_DIR/../../functions/index.sh
+
+RANCHER_DOMAIN="rancher.${HOSTNAME}.${_SANDBOX_ID}.instruqt.io"
+RANCHER_URL="https://${RANCHER_DOMAIN}"
+
+RANCHER_API_URL="${RANCHER_URL}/v3"
+RANCHER_ADMIN="admin"
+RANCHER_ADMIN_PASSWORD="$(tr -dc '[:alnum:]' </dev/urandom | head -c 13; echo '69')"
+
+########################## DELETE FOR PROD
+echo -e "\n\n\nthis is it ${RANCHER_ADMIN_PASSWORD}\n\n\n\n"
+########################  DELETE FOR PROD
+
+# Set variables for the instructions and passing down to other scripts
+agent variable set "PROD_CLUSTER_NAME" "$PROD_CLUSTER_NAME"
+agent variable set "RANCHER_ADMIN" "$RANCHER_ADMIN"
+agent variable set "RANCHER_ADMIN_PASSWORD" "$RANCHER_ADMIN_PASSWORD"
+
+agent variable set "OBSERVABILITY_URL" "$OBSERVABILITY_URL"
+agent variable set "OBSERVABILITY_USER" "$OBSERVABILITY_USERNAME"
+
+
+
+# Wait for cert-manager
+echo ">>> Waiting for cert-manager to be ready"
+kubectl wait --for=condition=Ready pod -l app.kubernetes.io/instance=cert-manager -n cert-manager --timeout=300s
+
+kubectl patch ingress rancher -n cattle-system --type='json' -p='[{"op": "replace", "path": "/spec/rules/0/host", "value": "'"${RANCHER_DOMAIN}"'"}, {"op": "replace", "path": "/spec/tls/0/hosts/0", "value": "'"${RANCHER_DOMAIN}"'"}]'
+# Wait for certificate to be issued
+kubectl wait --for=condition=Ready certificate rancher-tls -n cattle-system --timeout=300s
+
+rancher_first_login $RANCHER_URL $RANCHER_ADMIN_PASSWORD
