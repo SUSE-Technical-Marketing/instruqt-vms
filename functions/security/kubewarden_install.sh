28e14861f6741e418e7bee0de8bf341c505e2951
Jeroen van Erp <jeroen.vanerp@suse.com>
2025-11-04 13:35:02 +0100

    Add appco token and secret to kw install

--- functions/security/kubewarden_install.sh
+++ functions/security/kubewarden_install.sh
@@ -1,11 +1,16 @@
 install_kubewarden() {
+  appco_username=$1
+  appco_token=$2
+
   echo ">> Install Kubewarden Security Platform"
   helm repo add kubewarden https://charts.kubewarden.io
   helm repo update
 
   kubectl create namespace kubewarden
+  kubectl create secret docker-registry suse-application-collection --docker-server=dp.apps.rancher.io --docker-username=${appco_username} --docker-password=${appco_token} -n kubewarden
+
   helm install -n kubewarden kubewarden-crds kubewarden/kubewarden-crds
   helm install --wait -n kubewarden kubewarden-controller kubewarden/kubewarden-controller
 
-  helm upgrade -i --wait --namespace kubewarden --create-namespace kubewarden-defaults kubewarden/kubewarden-defaults --reuse-values
+  helm upgrade -i --wait --namespace kubewarden --create-namespace kubewarden-defaults kubewarden/kubewarden-defaults --reuse-values --set policyServer.imagePullSecret=suse-application-collection
 }
