25c7796f4a3a2040735ee12517514eb0dcbbc71a
Jeroen van Erp <jeroen@hierynomus.com>
2026-01-27 17:10:55 +0100

    Check certificate after download

--- functions/vm/ssl.sh
+++ functions/vm/ssl.sh
@@ -1,5 +1,14 @@
 download_gcp_certificate() {
+  local certFile=${1:-sandbox.crt}
+  local keyFile=${2:-sandbox.key}
+
   echo "Downloading SSL certificate and key from instance metadata"
-  curl -s -o sandbox.crt -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/ssl-certificate"
-  curl -s -o sandbox.key -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/ssl-certificate-key"
+  curl -s -o $certFile -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/ssl-certificate"
+  curl -s -o $keyFile -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/ssl-certificate-key"
+
+  if ! openssl x509 -in $certFile -noout &>/dev/null; then
+    echo "Error: Downloaded certificate is not valid"
+    exit 1
+  fi
+
 }
