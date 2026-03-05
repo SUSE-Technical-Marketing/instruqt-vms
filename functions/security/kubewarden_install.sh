db61b1ae6accd3d9bf5151aa30afaaf0615751be
Jeroen van Erp <jeroen@hierynomus.com>
2026-03-05 12:09:56 +0100

    Add new more pre-provisioned machine type
    Signed-off-by: Jeroen van Erp <jeroen@hierynomus.com>

--- functions/security/kubewarden_install.sh
+++ functions/security/kubewarden_install.sh
@@ -1,4 +1,13 @@
 install_kubewarden() {
+  echo ">> Install Kubewarden Security Platform"
+  helm repo add kubewarden https://charts.kubewarden.io
+  helm repo update
+
+  helm install -n kubewarden kubewarden-crds kubewarden/kubewarden-crds
+  helm install --wait -n kubewarden kubewarden-controller kubewarden/kubewarden-controller
+}
+
+install_kubewarden_defaults() {
   appco_username=$1
   appco_token=$2
 
@@ -7,15 +16,6 @@ install_kubewarden() {
     return 1
   fi
 
-  echo ">> Install Kubewarden Security Platform"
-  helm repo add kubewarden https://charts.kubewarden.io
-  helm repo update
-
-  kubectl create namespace kubewarden
   kubectl create secret docker-registry suse-application-collection --docker-server=dp.apps.rancher.io --docker-username=${appco_username} --docker-password=${appco_token} -n kubewarden
-
-  helm install -n kubewarden kubewarden-crds kubewarden/kubewarden-crds
-  helm install --wait -n kubewarden kubewarden-controller kubewarden/kubewarden-controller
-
   helm upgrade -i --wait --namespace kubewarden --create-namespace kubewarden-defaults kubewarden/kubewarden-defaults --reuse-values --set policyServer.imagePullSecret=suse-application-collection
 }
---
+++ vms/downstream/start.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# waits for Instruqt host bootstrap to finish
+until [ -f /opt/instruqt/bootstrap/host-bootstrap-completed ]
+do
+  sleep 1
+done
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+echo "Script directory: ${SCRIPT_DIR}"
+. $SCRIPT_DIR/../../functions/index.sh
+
+export KUBECONFIG=~/${MANAGER_HOSTNAME}-kubeconfig.yaml
+cat ~/${MANAGER_HOSTNAME}-kubeconfig.yaml
+
+# k8s_patch_traefik_gateway_with_host "\*.${HOSTNAME}.${_SANDBOX_ID}.instruqt.io"
+
+rancher_import_cluster ${HOSTNAME}
+CLUSTER_ID="$(rancher_return_clusterid ${HOSTNAME})"
+
+echo "Cluster ID: ${CLUSTER_ID}"
+
+# Get the ClusterRegistrationToken
+CLUSTER_TOKEN=$(rancher_return_clusterregistrationmanifest "${CLUSTER_ID}")
+
+echo "Cluster registration token manifest: ${CLUSTER_TOKEN}"
+# Switch to downstream cluster
+unset KUBECONFIG
+
+kubectl apply -f "${CLUSTER_TOKEN}"
+
+echo "Waiting for the cluster to be registered..."
+export KUBECONFIG=~/${MANAGER_HOSTNAME}-kubeconfig.yaml
+kubectl wait --for=condition=Ready --timeout=300s cluster.provisioning.cattle.io -n fleet-default ${HOSTNAME}
+
+# Switch to downstream cluster
+unset KUBECONFIG
+
+# Wait for the rancher-webhook deployment to be ready
+# Wait until the rancher-webhook deployment is present
+until kubectl get deployment rancher-webhook -n cattle-system &> /dev/null
+do
+  sleep 2
+done
+
+echo "Waiting for rancher-webhook deployment to be ready..."
+kubectl wait --for=condition=Available --timeout=300s deployment/rancher-webhook -n cattle-system
+
+if [ "${USE_INSTRUQT_SSL_CERTIFICATE:-false}" == "true" ]; then
+  echo ">>> Using Instruqt provided SSL certificate"
+  download_gcp_certificate sandbox.crt sandbox.key
+  k8s_create_wildcardtlssecret sandbox.crt sandbox.key wildcard-tls
+  echo ">>> Waiting for Rancher Wildcard TLS secret to be synced"
+  for i in {1..60}; do
+    if kubectl get secret wildcard-tls -n cattle-system &>/dev/null; then
+      echo "Rancher Wildcard TLS is ready"
+      break
+    fi
+    echo "Waiting for Rancher Wildcard TLS secret to be synced..."
+    sleep 5
+  done
+fi
