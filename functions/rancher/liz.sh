f9af866b16206e8f3a937113567bae8d167c80ad
Jeroen van Erp <jeroen@hierynomus.com>
2026-03-16 11:22:27 +0100

    debug liz install
    Signed-off-by: Jeroen van Erp <jeroen@hierynomus.com>

--- functions/rancher/liz.sh
+++ functions/rancher/liz.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 rancher_install_liz_ui() {
-  rancher_url=$1
-  bearer_token=$2
-  version=$3
+  local rancher_url=$1
+  local bearer_token=$2
+  local version=$3
+
+  echo "DEBUG: Installing Liz UI with version ${version} from ${rancher_url}"
+
   echo ">>> Installing Liz in the cluster"
   kubectl apply -f - <<EOF
 apiVersion: catalog.cattle.io/v1
@@ -17,25 +20,11 @@ EOF
 
   echo ">>> Waiting for Liz UI ClusterRepo to be downloaded"
   kubectl wait --for=condition=Downloaded clusterrepo/liz-ui-plugins --timeout=120s
-
   echo ">>> Installing Rancher AI UI from ClusterRepo"
   curl -sSL $rancher_url/v1/catalog.cattle.io.clusterrepos/liz-ui-plugins?action=install \
     -X POST \
     -H "Authorization: Bearer $bearer_token" \
-    -d - <<EOF
-{
-  "charts": [
-    {
-      "chartName":"rancher-ai-ui",
-      "version":"$version",
-      "releaseName":"rancher-ai-ui",
-      "annotations":{},
-      "values":{}
-    }
-  ],
-  "namespace":"cattle-ui-plugin-system"
-}
-EOF
+    -d "{\"charts\": [{\"chartName\":\"rancher-ai-ui\",\"version\":\"$version\",\"releaseName\":\"rancher-ai-ui\",\"annotations\":{},\"values\":{}}],\"namespace\":\"cattle-ui-plugin-system\"}"
   if [ $? -ne 0 ]; then
     echo "ERROR: Failed to install Liz UI"
     exit 1
