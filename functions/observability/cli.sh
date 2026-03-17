2c5b9385c7797b850fe19f33f73c140dc6b61057
Jeroen van Erp <jeroen@hierynomus.com>
2026-03-17 10:34:31 +0100

    Higher version CLI
    Signed-off-by: Jeroen van Erp <jeroen@hierynomus.com>

--- functions/observability/cli.sh
+++ functions/observability/cli.sh
@@ -4,7 +4,7 @@
 # Install the SUSE Observability CLI
 #######################################
 observability_install_cli() {
-  local version=${1:-3.1.1}
+  local version=${1:-3.2.2}
   if ! [ -x "$(command -v sts)" ]; then
     curl -s -o- https://dl.stackstate.com/stackstate-cli/install.sh | STS_CLI_LOCATION=/usr/local/bin STS_CLI_VERSION=$version bash
   else
