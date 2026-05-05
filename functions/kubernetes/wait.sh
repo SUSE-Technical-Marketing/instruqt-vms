36d87f55d6ae6756ac20981dcdc8d476d28ca15d
Jeroen van Erp <jeroen@hierynomus.com>
2026-05-05 11:56:56 +0200

    Add wait for k8s
    Signed-off-by: Jeroen van Erp <jeroen@hierynomus.com>

---
+++ functions/kubernetes/wait.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wait_for_kubernetes() {
+  echo "Waiting for Kubernetes API to be available..."
+  RETRIES=30
+  COUNT=0
+  until kubectl version --request-timeout=5s &> /dev/null
+  do
+    sleep 2
+    COUNT=$((COUNT+1))
+    if [ $COUNT -ge $RETRIES ]; then
+      echo "Kubernetes API is not available after $RETRIES attempts."
+      return 1
+    fi
+  done
+}
